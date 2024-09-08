bcac035b29b72c54bc665f1d5ec2cc26385d040f
kashif <mohd.kashif@sabpaisa.in>
2024-09-08 12:06:50 +0530

    Added changes by kashif

--- test.js
+++ test.js
@@ -9,28 +9,3 @@ let e = 30
 //Second change
 
 
-//code by share
-let f=20;
-let g=20;
-let h=f+g;
-
-
-let i = 30
-
-let j=20;
-let k=20;
-let l=j+k;
-
-
-let m = 30
-
-let n=20;
-let o=20;
-let p=o+b;
-
-
-let s = 30
-
-
-
-console.log("This change is done by kashif")
